de78b7c12694dbdfc16b63871390f14ca48187cb
Dominic Barajas <dominicbarajas548@gmail.com>
2020-10-19 18:12:57 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,5 +29,5 @@ def select_character_names_and_number_of_books_they_are_in
   JOIN character_books ON characters.id = character_books.character_id
   JOIN books ON character_books.book_id = books.id
   GROUP BY characters.name
-  ORDER BY books.id ASC;"
+  ORDER BY books.id DESC;"
 end
